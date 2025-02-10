38afc9832366baf565415857a5a38f82e8f11f78
Yusuf Coban <coban@schnaithmann.de>
2025-02-10 13:53:44 +0100

    asdasd

--- NodejsConsoleApp1/modi.js
+++ NodejsConsoleApp1/modi.js
@@ -2,12 +2,6 @@ const venom = require('venom-bot');
 const fs = require('fs');
 const path = require('path');
 
-// Directory setup
-const directories = ['logs', 'qrcodes', 'images', 'videos', 'memos'];
-directories.forEach(dir => {
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-});
-
 const groupCategories = {
     "blitzer_rems_murr": [
         '4915140440209-1547593893@g.us',
@@ -37,14 +31,11 @@ const groupCategories = {
     ]
 };
 
-const keywords2 = ["blitzer", "tempo", "radar", "polizei", "poko", "anhänger", "foto",
+const positivFilter = ["blitzer", "tempo", "radar", "polizei", "poko", "anhänger", "foto",
     "geschwindig", "kontrolle", "verkehrskontrolle", "zivil", "bilder", "pk", "container", "läster", "bitzer",
     "laser", "kasten", "box", "30", "70", "80", "100", "120", "bus", "schritt", "b29", "b14"
 ];
-const keywords3 = ["löschen", "beobachtet","löschen"];
-
-
-let lastMessages = {};
+const negativeFilter = ["löschen", "beobachtet"];
 
 venom.create({
     session: 'session-name',
@@ -68,23 +59,18 @@ function start(client) {
         try {
             for (const category in groupCategories) {
                 if (groupCategories[category].includes(message.from)) {
-                    if (!lastMessages[category]) {
-                        lastMessages[category] = [];
-                    }
-
-                    if (message.type === 'chat' && containsKeyword(message.body, keywords2) && !containsKeyword(message.body, keywords3) && !isDuplicateMessage(category, message.body)) {
-                        updateLastMessages(category, message.body); // Add message before forwarding
-                        console.log('----------------START MESSAGE-------(' + category + ')---------------------');
-                        console.log('Current lastMessages:', lastMessages[category]);
-
+                    if (message.type === 'chat' && containsKeyword(message.body, positivFilter) && !containsKeyword(message.body, negativeFilter) && isNotRolli(message.author)) {
+                        console.log('----------------START MESSAGE-------(' + category + ')----');
                         const targetGroupIds = groupCategories[category].filter(id => id !== message.from);
                         await Promise.all(targetGroupIds.map(async groupId => {
-                            if (checkIfPass(groupId, message.body, message.from, message.author)) {
-                                await client.sendText(groupId, message.body);
-                                console.log(`Text message forwarded to ${groupId}:`, message.body);
-                            }
+                            await client.sendText(groupId, message.body);
+                            const truncatedBody = message.body.length > 10 ? message.body.substring(0, 10) + '...' : message.body;
+                            console.log(`Text message forwarded to ${groupId}:`, truncatedBody);
                         }));
                         console.log('----------------END MESSAGE----------------------------');
+                    }
+                    else if (message.type === 'chat' && isNotRolli(message.author)) {
+                        console.log('---------Rollis Nachrichten werden ignoriert-----------');
                     }
                 }
             }
@@ -95,28 +81,16 @@ function start(client) {
     process.stdin.resume();
 }
 
-function normalizeMessage(message) {
-    return message.trim().toLowerCase().replace(/\s+/g, ' ').slice(0, 25);
-}
 
-function isDuplicateMessage(category, message) {
-    const normalizedMessageCurrent = normalizeMessage(message);
-    return lastMessages[category]?.some(msg => msg === normalizedMessageCurrent);
-}
-
-function updateLastMessages(category, newMessage) {
-    const normalizedMessage = normalizeMessage(newMessage);
-    lastMessages[category].push(normalizedMessage);
-    if (lastMessages[category].length > 30) {
-        lastMessages[category].shift();
-    }
-}
-
-function checkIfPass(empfaenger, message, sender, author) {
+function isNotRolli(author) {
     return author !== "491717560044@c.us"; // Rolli schickt alle selbst ab!
 }
 
 function containsKeyword(text, keywords) {
-    const cleanedText = text.replace(/\s+/g, '').toLowerCase();
-    return keywords.some(keyword => cleanedText.includes(keyword.toLowerCase()));
+    const cleanedText = text.replace(/\s+/g, '').toLowerCase(); // Remove spaces and convert to lowercase 
+    return keywords.some(keyword => {
+        const cleanedKeyword = keyword.replace(/\s+/g, '').toLowerCase();
+        return cleanedText.includes(cleanedKeyword); // Allow partial matches anywhere in the text
+    });
 }
+
